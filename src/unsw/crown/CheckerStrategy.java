954f6c6fd5ceeef875bde41666b2664df8497ee4
Braedon Wooding <b.wooding@unsw.edu.au>
2021-06-21 08:28:22 +1000

    Update src/unsw/crown/CheckerStrategy.java

--- src/unsw/crown/CheckerStrategy.java
+++ src/unsw/crown/CheckerStrategy.java
@@ -9,5 +9,5 @@ public interface CheckerStrategy {
     // what about rendering?
     // maybe some sort of 'renderSpecial' to add the crowns/duck icons.
 
-    public List<Position> validPositions(Checkerboard board, Position position);
+    public void addValidPositions(List<Position> validPositions, Checkerboard board, Position position, Color color);
 }
