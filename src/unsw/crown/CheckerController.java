777a13ba6f5c442a1b635fadc70c355c97742ab2
Braedon Wooding <b.wooding@unsw.edu.au>
2021-06-21 08:27:13 +1000

    Update src/unsw/crown/CheckerController.java

--- src/unsw/crown/CheckerController.java
+++ src/unsw/crown/CheckerController.java
@@ -137,13 +137,14 @@ public class CheckerController {
 
     private boolean recursivelyAddCheckerPositions(Checker checker, Position source) {
         List<Position> positions = checker.validPositions(board, source);
+        int validJumps = 0;
         for (Position pos : positions) {
             possiblePredecessors.putIfAbsent(pos, new HashSet<>());
             possiblePredecessors.get(pos).add(source);
 
             // check if this jumps over a piece
             Checker c = board.getPieceAt(Position.midPointPosition(source, pos));
-            if (c != null) {
+            if (c != null && c != checker) {
                 String color;
                 if (recursivelyAddCheckerPositions(checker, pos)) {
                     color = "silver";
@@ -151,11 +152,12 @@ public class CheckerController {
                     color = "purple";
                 }
                 addPossiblePosition(pos, color);
-            } else {
+                validJumps++;
+            } else if (board.getPieceAt(source) != null) {
                 addPossiblePosition(pos, "purple");
             }
         }
-        return positions.size() > 0;
+        return validJumps++;
     }
 
     private void addPossiblePosition(Position pos, String color) {
